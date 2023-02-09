7bfe84d3975004e1821a0361d5319b8d9194de12
paula-cano <paula.14canob@gmail.com>
2023-02-09 09:16:45 -0500

    se agrega app.js con funcion de resta y el archivo .gitignore

---
+++ app.js
@@ -0,0 +1,17 @@
+let n1 =7;
+let n2=3;
+
+
+/**
+ * Restar(num1, num2) toma dos números como argumentos y devuelve el resultado de restar el segundo
+ * número al primero.
+ * @param num1 - El primer número a restar.
+ * @param num2 - número
+ * @returns El resultado de la operación
+ */
+function restar(num1, num2){
+    let resultado = num1-num2;
+    console.log (`el resultado de la resta es ${resultado}`)
+    return resultado
+}
+restar (n1,n2);
